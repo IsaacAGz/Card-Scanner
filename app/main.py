e2e276373bfa04d9ab87353d1ebe1b9e025f3324
Isaac Angulo <isaac.angulogomez@gmail.com>
2026-06-15 20:16:10 -0700

    Sync endpoint to download and embed new sets

--- app/main.py
+++ app/main.py
@@ -1,7 +1,11 @@
-from fastapi import FastAPI, UploadFile, File, status, HTTPException
+from fastapi import FastAPI, UploadFile, File, status, HTTPException, Header, Security
 from transformers import AutoImageProcessor, AutoModel
 from contextlib import asynccontextmanager
 from ultralytics import YOLO
+from dotenv import load_dotenv
+import requests
+import time
+import urllib.request
 import torch
 import os
 import faiss
@@ -15,6 +19,10 @@ model = None
 processor = None
 index = None
 db_conn = None
+
+load_dotenv()
+
+API_KEY = os.getenv("ADMIN_KEY")
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -73,12 +81,54 @@ def get_embedding(image_np):
     '''
 
     '''
-
     rgb_image = cv2.cvtColor(image_np, cv2.COLOR_BGR2RGB)
     inputs = processor(images=rgb_image, return_tensors="pt").to(model.device)
     with torch.no_grad():
         outputs = model(**inputs)
     return outputs.last_hidden_state[:,0,:].cpu().numpy()
+
+def sync_new_cards(set_code: str, faiss_index, sqlite_conn, model, processor):
+    '''
+
+    '''
+    new_cards = fetch_new_cards(set_code)
+    print(f"Found {len(new_cards)} new cards to index.")
+
+    cursor = sqlite_conn.cursor()
+
+    for card in new_cards:
+        cursor.execute("SELECT 1 FROM cards WHERE name = ? AND set_code = ?", (card['name'], card['set_code']))
+
+        if cursor.fetchone():
+            continue
+
+        try:
+            resp = urllib.request.urlopen(card['image_url'])
+            img_array = np.array(bytearray(resp.read()), dtype=np.uint8)
+            card_img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
+
+            rgb_image = cv2.cvtColot(card_img, cv2.COLOR_BGR2RGB)
+            inputs = processor(images=rgb_image, return_tensors="pt").to(model.device)
+
+            with torch.no_grad():
+                outputs = model(**inputs)
+
+            vector = outputs.last_hidden_state[:, 0, :].cpu().numpy().astype('float32')
+
+            next_faiss_id = faiss_index.ntotal
+            faiss_index.add(vector.reshape(1, -1))
+
+            cursor.execute("""
+                INSERT INTO cards(faiss_id, name, set_code)
+                VALUES (?, ?, ?)
+                """, (next_faiss_id, card['name'], card['set_code']))
+            
+            print(f"Succesfully indexed {card['name']} [{card['set_code'].upper()}]")
+
+        except Exception as e:
+            print(f"Failed to process card{card['name']}: {e}")
+            continue
+                
 
 def process_image(frame):
     '''Crops image, performs vector embedding, to retrieve card attributes.
@@ -89,10 +139,10 @@ def process_image(frame):
     Returns:
         Dictionary: containing number of cards and and the information of the cards obtained
     '''
-
     results = yolo(frame, save=True, conf=.75)
 
     found_cards_info = []
+    last_seen_cards = {}
 
     height, width, _ = frame.shape
 
@@ -111,6 +161,15 @@ def process_image(frame):
         
         if card_crop.size == 0: continue
 
+        # Cachin for recently scanned cards
+        gray_crop = cv2.cvtColor(card_crop, cv2.COLOR_BGR2GRAY)
+        small_crop = cv2.resize((gray_crop), (8,8))
+        card_hash = hash(small_crop.tobytes())
+
+        if card_hash in last_seen_cards:
+            found_cards_info.append(last_seen_cards[card_hash])
+            continue
+
         # FAISS ID embedding vector
         vector = get_embedding(card_crop) 
 
@@ -125,7 +184,7 @@ def process_image(frame):
         card_idx = indices[0][0]
         card_info = get_card_info(card_idx)
 
-        # Obtain card attributes if database card close enough to corpped card
+        # Append found card and card attributes if database card close enough to cropped embedded card
         found_cards_info.append({
             "name": card_info[0] if card_info else "Unknown",
             "set": card_info[1] if card_info else "Unknown",
@@ -133,13 +192,82 @@ def process_image(frame):
             "box": [xmin, ymin, xmax, ymax]
         })
 
+        # Get card info for cashing
+        card_data = {
+            "name": card_info[0],
+            "set": card_info[1],
+            "box": [xmin, ymin, xmax, ymax]
+        }
+        
+        last_seen_cards[card_hash] = card_data
+
     return {"count": len(found_cards_info), "cards": found_cards_info}
+
+def fetch_new_cards(set_code: str):
+    '''
+    Helper funciton that performs get request to srycall API to fetch all cards from selected 
+    set and returns as a list of dictionaries (cards) to be downloaded and embedded.
+
+    Args:
+        set_code: string that matches a MTG set, usually newly released sets.
+
+    Returns:
+        cards_to_index: list of dictionaries containing the card data for all cards in the set.
+    '''
+    url = "https://api.scryfall.com/cards/search?q=set:{set_code.lower()}+is:unique"
+    headers = {"User-Agent": "GRXSCardScannerMicro-service", "Accept": "application/json"}
+
+    cards_to_index = []
+
+    while url:
+        response = response.get(url, headers=headers)
+
+        # Too many requests
+        if response.status_code == 429:
+            time.sleep(2)
+            continue
+        
+        data = response.json()
+
+        # Iterate through all cards to see if eligible to embedd and retrieve all data and uri for card image.
+        for card in data.get('data', []):
+            if 'image_uris' in card and 'border_crop' in card['image_uris'] and 'paper' in card['games']:
+                cards_to_index.append({
+                    "id": card['mtgo_id'],
+                    "name": card['name'],
+                    "set_code": card['set'],
+                    "image_uri": card['image_uris']['border_crop']
+                })
+        
+        url = data.get('next_page') if data.get('has_mode') else None
+        time.sleep(0.1)
+
+    return cards_to_index
 
 
 @app.get("/health")
 async def health():
     '''Health check for API'''
     return {"status": "healthy"}
+
+@app.post("/admin/sync-set")
+async def sync_set(set_code: str, x_api_key: str = Header(...)):
+    '''
+    Admin endpoint to sync set cards to index db
+
+    Args:
+        set_code: string specifying desired set to sync
+        x_api_key: string to verify admin, stored in header
+    '''
+    
+    if x_api_key != API_KEY:
+        raise HTTPException(status_code=401, details="Unauthorized execution attempt.")
+    
+    sync_new_cards(set_code, index, db_conn, model, processor)
+
+    get_card_info.cache_clear()
+
+    return {"message": f"Set {set_code.upper()} successfully, synced into lookup space."}
 
 
 @app.post("/scan", status_code=status.HTTP_200_OK)
